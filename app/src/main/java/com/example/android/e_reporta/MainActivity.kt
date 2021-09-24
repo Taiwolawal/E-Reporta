477a3f5774daebd31e35a65e8a8fccdfe207e9fc
Taiwo Lawal <taiwolawal360@gmail.com>
2021-09-24 09:53:53 +0100

    - Initial project

---
+++ app/src/main/java/com/example/android/e_reporta/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.e_reporta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_EReporta)
+        setContentView(R.layout.activity_main)
+    }
+}
