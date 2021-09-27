0bb5ac2ad92dab829eacd7e2489b0039cbd6f2d2
Taiwo Lawal <taiwolawal360@gmail.com>
2021-09-27 17:57:44 +0100

    - Basic sign up and detailed sign up layout created.

---
+++ app/src/main/java/com/example/android/e_reporta/BasicSignUpFragment.kt
@@ -0,0 +1,22 @@
+package com.example.android.e_reporta
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BasicSignUpFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_basic_sign_up, container, false)
+    }
+
+}
